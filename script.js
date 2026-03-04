b8f08dbbd37ad9d3586a0bd130f0c177b6b5758a
jangri09 <jangri09@gvgs.no>
2026-03-04 11:09:25 +0100

    feat: Initialize game board with random bomb placement logic and update grid cell styling with new borders.

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
     const grid = document.querySelector('.grid')
+    const flagsLeft = document.querySelector('#flags-left')
     const width = 10
+    let bombAmount = 20
 
-    console.log(grid);
+
 
     //Create Board
     function createBoard() {
+        flagsLeft.innerHTML = bombAmount
+        //get random bombs
+        const bombsArray = Array(bombAmount).fill('bomb')
+        const emptyArray = Array(width * width - bombAmount).fill('valid')
+        const gameArray = emptyArray.concat(bombsArray)
+        const shuffledArray = gameArray.sort(() => Math.random() - 0.5)
 
-
-
+        //create squares
         for (let i = 0; i < width * width; i++) {
             const square = document.createElement('div')
             square.id = i
+            square.classList.add(shuffledArray[i])
             grid.appendChild(square)
+            squares.push(square)
+
+
         }
     }
     createBoard()
