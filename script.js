455b53f6ce338a94025ba1c51206b1e9499a96a2
jangri09 <jangri09@gvgs.no>
2026-03-04 10:35:54 +0100

    feat: Implement initial 10x10 game board creation with basic styling for grid and cells.

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
     const grid = document.querySelector('.grid')
+    const width = 10
 
     console.log(grid);
+
+    //Create Board
+    function createBoard() {
+
+
+
+        for (let i = 0; i < width * width; i++) {
+            const square = document.createElement('div')
+            square.id = i
+            grid.appendChild(square)
+        }
+    }
+    createBoard()
 })
